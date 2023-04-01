f7d54de2c9df4ada5659626fbbe336ade634614b
Jorge Silva <64603266+jesv99@users.noreply.github.com>
2023-04-01 10:38:17 -0400

    Add files via upload

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,6 +1,6 @@
+using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,16 +10,13 @@ namespace EsepWebhook;
 public class Function
 {
     
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    
+    public string FunctionHandler(object input, ILambdaContext context)
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
         var client = new HttpClient();
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
